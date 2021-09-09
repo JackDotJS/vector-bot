45e1567cdf4086c1924a99d47cd3e35150a63b1c
Kyle Edwards <wingedasterisk@gmail.com>
2021-09-08 20:04:28 -0700

    boot manager: move envopts to separate module

--- vmodules/base/bmanager.js
+++ vmodules/base/bmanager.js
@@ -2,29 +2,14 @@
  * VECTOR :: BOOT MANAGER
  */
 
-console.clear();
-
 const child = require(`child_process`);
 const readline = require(`readline`);
 const fs = require(`fs`);
 const util = require(`util`);
 const AZip = require(`adm-zip`);
 const pkg = require(`../../package.json`);
-
+const opts = require(`./envopts.js`);
 const log = require(`./log.js`);
-
-process.title = `Vector Bot ${pkg.version} | Initializing...`;
-
-const opts = {
-  debug: false,
-  log: {
-    stream: null,
-    filename: null,
-  },
-  recovery: {
-    log: null
-  }
-};
 
 // ensures bot doesnt end up in a boot loop
 const resetcheck = {
@@ -43,8 +28,8 @@ const resetcheck = {
 module.exports = (data) => {
   opts.debug = data.debug;
 
-  init();
+  preinit();
 };
 
-function init() {
+function preinit() {
 }
---
+++ vmodules/base/envopts.js
@@ -0,0 +1,10 @@
+module.exports = {
+  debug: false,
+  log: {
+    stream: null,
+    filename: null,
+  },
+  recovery: {
+    log: null
+  }
+};
