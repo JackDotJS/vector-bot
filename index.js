469d94ca90082d53798afa6dc6bf00e80d2250e7
Kyle Edwards <wingedasterisk@gmail.com>
2021-09-15 17:40:12 -0700

    significant amount of changes to make crash recovery actually work

--- index.js
+++ index.js
@@ -6,9 +6,24 @@ const child = require(`child_process`);
 const fs = require(`fs`);
 const readline = require(`readline`);
 const pkg = require(`./package.json`);
+const log = require(`./vmodules/util/logger.js`).write;
+
+// ensures bot doesnt end up in a boot loop
+const resetcheck = {
+  resets: 0,
+  hour: 0,
+  check: setInterval(() => {
+    const now = new Date().getHours();
+    if (now !== resetcheck.hour) {
+      resetcheck.resets = 0;
+      resetcheck.hour = now;
+    }
+  }, 1000)
+};
 
 const opts = {
-  debug: false
+  debug: false,
+  recovered: false,
 };
 
 process.title = `Vector Bot ${pkg.version}`;
@@ -32,21 +47,17 @@ for (const item of mkdirs) {
   }
 }
 
-// theres probably a better way to do this
+const setup_qs = [];
 
-(function q1() {
-  console.clear();
-
+setup_qs.push((resolve, reject) => {
   cli.question(`START VECTOR [Y/N]`, (res) => {
-    if (res.trim().toLowerCase() === `y`) return q2();
+    if (res.trim().toLowerCase() === `y`) return resolve();
     if (res.trim().toLowerCase() === `n`) return process.exit();
-    q1();
+    reject();
   });
-})();
+});
 
-function q2() {
-  console.clear();
-
+setup_qs.push((resolve, reject) => {
   console.log([
     `Debug mode enables "DEBUG" level log messages and places the bot into a "development mode", which only allows input from whitelisted users.`,
     ``
@@ -55,22 +66,129 @@ function q2() {
   cli.question(`ENABLE DEBUG MODE [Y/N]`, (res) => {
     if (res.trim().toLowerCase() === `y`) {
       opts.debug = true;
-      return start();
+      return resolve();
     }
     if (res.trim().toLowerCase() === `n`) return start();
-    q2();
+    reject();
   });
-}
+});
+
+(async function setup() {
+  for (let i = 0; i < setup_qs.length; null) {
+    console.clear();
+    try {
+      await new Promise(setup_qs[i]);
+      i++;
+    }
+    catch (err) {
+      // damn bro that sucks
+    }
+  }
+
+  cli.close();
+  start();
+})();
 
 function start() {
-  cli.close();
+  const cfg = require(`./vmodules/util/bot_config.js`)(opts.debug);
 
-  // this was only really done to pass options slightly easily
-  // using a child process for this is probably immensely overkill
-  // idk who cares lol
+  if (!opts.debug) {
+    // if the bot has restarted more than x times within the past hour, print a warning.
+    if (resetcheck.resets >= cfg.resetcheck.min_reset_warning) {
+      log(`[SETUP]: Unusually high client reset count: ${resetcheck.resets}`, `warn`);
+    }
+
+    // if the bot has restarted more than x times within the past hour, stop.
+    if (resetcheck.resets >= cfg.resetcheck.max_resets_per_hour) {
+      log(`[SETUP]: Potential boot loop detected, shutting down for safety.`, `warn`);
+      return exit(18);
+    }
+  }
+
   const sm = child.spawn(`node`, [`vmodules/core/bootstrap.js`, JSON.stringify(opts)], {
     stdio: `inherit`
   });
 
-  sm.on(`exit`, process.exit);
+  sm.on(`exit`, exit);
+}
+
+function exit(code) {
+  // env.api.send({ t: `STOP` }, (err) => {
+  //   if (err) log(`Failed to send payload to API subprocess: ${err.stack}`, `error`);
+  // });
+
+  resetcheck.resets++;
+
+  let exit = true;
+  let timeout = 500;
+  let report = true;
+
+  switch (code) {
+    case 0:
+      log(`Vector is now shutting down at user request.`, `info`);
+      report = false;
+      break;
+    case 1:
+      log(`Vector seems to have crashed. Restarting...`, `info`);
+      exit = false;
+      if (opts.debug) timeout = 5000;
+      break;
+    case 2:
+      log(`Bash Error.`, `fatal`);
+      break;
+    case 3:
+      log(`Internal JavaScript parse error.`, `fatal`);
+      break;
+    case 4:
+      log(`Internal JavaScript Evaluation Failure.`, `fatal`);
+      break;
+    case 5:
+      log(`Fatal Error.`, `fatal`);
+      break;
+    case 6:
+      log(`Non-function Internal Exception Handler.`, `fatal`);
+      break;
+    case 7:
+      log(`Internal Exception Handler Run-Time Failure.`, `fatal`);
+      break;
+    case 8:
+      log(`Uncaught exception.`, `fatal`);
+      break;
+    case 9:
+      log(`Invalid Launch Argument(s).`, `fatal`);
+      break;
+    case 10:
+      log(`Internal JavaScript Run-Time Failure.`, `fatal`);
+      break;
+    case 12:
+      log(`Invalid Debug Argument(s).`, `fatal`);
+      break;
+    case 16:
+      log(`Vector is now restarting at user request...`, `info`);
+      exit = false;
+      report = false;
+      break;
+    case 17:
+      log(`Vector is now undergoing scheduled restart.`, `info`);
+      exit = false;
+      report = false;
+      break;
+    case 18:
+      log(`Vector is shutting down automatically.`, `fatal`);
+      break;
+  }
+
+  if (code > 128) log(`Signal exit code ${code - 128}.`, `fatal`);
+
+  setTimeout(() => {
+    if (report) {
+      opts.recovered = true;
+    }
+
+    if (exit) {
+      return process.exit(code);
+    }
+
+    start();
+  }, timeout);
 }
--- vmodules/core/bootstrap.js
+++ vmodules/core/bootstrap.js
@@ -2,29 +2,7 @@
  * VECTOR :: BOOTSTRAPPER
  */
 
-const child = require(`child_process`);
-const fs = require(`fs`);
-const util = require(`util`);
-const AZip = require(`adm-zip`);
-const djs = require(`discord.js`);
-const keys = require(`../../cfg/keys.json`);
-const cfg = require(`../../cfg/bot.json`);
-const pkg = require(`../../package.json`);
-const logger = require(`../util/logger.js`);
-const log = logger.write;
-
-// ensures bot doesnt end up in a boot loop
-const resetcheck = {
-  resets: 0,
-  hour: 0,
-  check: setInterval(() => {
-    const now = new Date().getHours();
-    if (now !== resetcheck.hour) {
-      resetcheck.resets = 0;
-      resetcheck.hour = now;
-    }
-  }, 1000)
-};
+console.clear();
 
 const env = {
   debug: false,
@@ -38,61 +16,51 @@ const env = {
   }
 };
 
-console.log(process.argv[2]);
-
 const setup_opts = JSON.parse(process.argv[2]);
 env.debug = setup_opts.debug;
 
-function preinit() {
-  console.clear();
+const child = require(`child_process`);
+const fs = require(`fs`);
+const util = require(`util`);
+const AZip = require(`adm-zip`);
+const djs = require(`discord.js`);
+const keys = require(`../../cfg/keys.json`);
+const cfg = require(`../util/bot_config.js`)(env.debug);
+const pkg = require(`../../package.json`);
+const logger = require(`../util/logger.js`);
+const log = logger.write;
 
-  process.title = `Vector Bot ${pkg.version} | Initializing...`;
+process.title = `Vector Bot ${pkg.version} | Initializing...`;
 
-  env.log.filename = new Date().toUTCString().replace(/[/\\?%*:|"<>]/g, `.`);
-  env.log.stream = fs.createWriteStream(`./logs/all/${env.log.filename}.log`);
+env.log.filename = new Date().toUTCString().replace(/[/\\?%*:|"<>]/g, `.`);
+env.log.stream = fs.createWriteStream(`./logs/all/${env.log.filename}.log`);
 
-  logger.opts.stream = env.log.stream;
-  logger.opts.debug = env.debug;
+logger.opts.stream = env.log.stream;
+logger.opts.debug = env.debug;
 
-  if (!env.debug) {
-    // if the bot has restarted more than x times within the past hour, print a warning.
-    if (resetcheck.resets >= cfg.resetcheck.min_reset_warning) {
-      log(`[PREINIT]: Unusually high client reset count: ${resetcheck.resets}`, `warn`);
-    }
+env.api = child.spawn(`node`, [`vmodules/core/api.js`, env.debug], {
+  stdio: [`inherit`, `inherit`, `inherit`, `ipc`]
+});
 
-    // if the bot has restarted more than x times within the past hour, stop.
-    if (resetcheck.resets >= cfg.resetcheck.max_resets_per_hour) {
-      log(`[PREINIT]: Potential boot loop detected, shutting down for safety.`, `warn`);
-      return exit(18);
-    }
+env.api.on(`message`, (data) => {
+  if (data == null || data.constructor !== Object || data.t == null) {
+    return log(util.inspect(data)); // to the debugeon with you
   }
 
-  env.api = child.spawn(`node`, [`vmodules/core/api.js`, env.debug], {
-    stdio: [`inherit`, `inherit`, `inherit`, `ipc`]
-  });
+  switch (data.t) {
+    case `LOG`:
+      log(`[API] ${data.c.content}`, data.c.level, data.c.file);
+      break;
+    default:
+      log(util.inspect(data)); // to the debugeon with you... again
+  }
+});
 
-  env.api.on(`message`, (data) => {
-    if (data == null || data.constructor !== Object || data.t == null) {
-      return log(util.inspect(data)); // to the debugeon with you
-    }
-
-    switch (data.t) {
-      case `LOG`:
-        log(`[API] ${data.c.content}`, data.c.level, data.c.file);
-        break;
-      default:
-        log(util.inspect(data)); // to the debugeon with you... again
-    }
-  });
-
-  env.api.on(`exit`, (code) => {
-    if (code != 0) {
-      log(`API subprocess closed prematurely!`, `error`);
-    }
-  });
-
-  init();
-}
+env.api.on(`exit`, (code) => {
+  if (code != 0) {
+    log(`API subprocess closed prematurely!`, `error`);
+  }
+});
 
 function init() {
   log(`Launching Vector...`, `info`);
@@ -106,7 +74,7 @@ function init() {
   const manager = new djs.ShardingManager(`./vmodules/core/shard.js`, shard_opts);
 
   manager.on(`shardCreate`, shard => {
-    log(`[S-${shard.id}] New shard created!`, `info`);
+    log(`[SHARD ${shard.id}] New shard created!`, `info`);
 
     shard.on(`message`, (data) => {
       if (data == null || data.constructor !== Object || data.t == null) {
@@ -139,98 +107,12 @@ function init() {
       }
     });
 
-    shard.on(`death`, () => {
-      log(`he's dead jim`);
+    shard.on(`death`, p => {
+      log(`[SHARD ${shard.id}] Process ended with exit code ${p.exitCode}`, `warn`);
     });
   });
 
   manager.spawn();
 }
 
-function exit(code) {
-  env.api.send({ t: `STOP` }, (err) => {
-    if (err) log(`Failed to send payload to API subprocess: ${err.stack}`, `error`);
-  });
-
-  resetcheck.resets++;
-  env.log.stream.end();
-
-  let exit = true;
-  let timeout = 500;
-  let report = true;
-
-  switch (code) {
-    case 0:
-      log(`Vector is now shutting down at user request.`, `info`);
-      report = false;
-      break;
-    case 1:
-      log(`Vector seems to have crashed. Restarting...`, `info`);
-      exit = false;
-      if (env.debug) timeout = 5000;
-      break;
-    case 2:
-      log(`Bash Error.`, `fatal`);
-      break;
-    case 3:
-      log(`Internal JavaScript parse error.`, `fatal`);
-      break;
-    case 4:
-      log(`Internal JavaScript Evaluation Failure.`, `fatal`);
-      break;
-    case 5:
-      log(`Fatal Error.`, `fatal`);
-      break;
-    case 6:
-      log(`Non-function Internal Exception Handler.`, `fatal`);
-      break;
-    case 7:
-      log(`Internal Exception Handler Run-Time Failure.`, `fatal`);
-      break;
-    case 8:
-      log(`Uncaught exception.`, `fatal`);
-      break;
-    case 9:
-      log(`Invalid Launch Argument(s).`, `fatal`);
-      break;
-    case 10:
-      log(`Internal JavaScript Run-Time Failure.`, `fatal`);
-      break;
-    case 12:
-      log(`Invalid Debug Argument(s).`, `fatal`);
-      break;
-    case 16:
-      log(`Vector is now restarting at user request...`, `info`);
-      exit = false;
-      report = false;
-      break;
-    case 17:
-      log(`Vector is now undergoing scheduled restart.`, `info`);
-      exit = false;
-      report = false;
-      break;
-    case 18:
-      log(`Vector is shutting down automatically.`, `fatal`);
-      break;
-  }
-
-  if (code > 128) log(`Signal exit code ${code - 128}.`, `fatal`);
-
-  setTimeout(() => {
-    if (report) {
-      const crashlog = `../../logs/crash/${env.log.filename}.log`;
-
-      fs.copyFileSync(`../../logs/all/${env.log.filename}.log`, crashlog);
-
-      env.recovery.log = crashlog;
-    }
-
-    if (exit) {
-      return process.exit(code);
-    }
-
-    preinit();
-  }, timeout);
-}
-
-preinit();
+init();
