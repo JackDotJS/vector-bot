a8b7803e630327b2111add9c4f6e731e894420f3
Kyle Edwards <wingedasterisk@gmail.com>
2021-09-19 22:16:47 -0700

    start of first command ?about

---
+++ vmodules/commands/about.js
@@ -0,0 +1,21 @@
+const djs = require(`discord.js`);
+const memory = require(`../core/shard_memory.js`);
+const Command = require(`../classes/command.js`);
+const log = require(`../util/logger.js`).write;
+
+const metadata = {
+  name: `about`,
+  description: {
+    short: `A quick introduction to Vector.`,
+    long: `Provides a quick introduction to Vector.`
+  },
+  dm: true,
+  perm: `SEND_MESSAGES`,
+  run: null,
+};
+
+metadata.run = async (m) => {
+
+};
+
+module.exports = new Command(metadata);
