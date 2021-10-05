35e7fe872bffd86d5c4241c7cc52397e1316480c
Kyle Edwards <wingedasterisk@gmail.com>
2021-10-04 17:17:35 -0700

    config manager: add get() method

--- vmodules/managers/config_manager.js
+++ vmodules/managers/config_manager.js
@@ -2,11 +2,23 @@
  * VECTOR :: GUILD CONFIG MANAGER
  */
 
+const fetch = require(`node-fetch`);
 const memory = require(`../core/shard_memory.js`);
 const log = require(`../util/logger.js`).write;
 
 module.exports = class ConfigManager {
   constructor() {
     throw new Error(`Why are you doing this? (Cannot instantiate this class.)`);
+  }
+
+  static async get(id) {
+    if (id == null || typeof id !== `string` || id.length === 0) return null;
+
+    const bot = memory.client;
+
+    const res = await fetch(`localhost:${bot.cfg.api.port}/guildcfg?key=${bot.keys.db}&guild=${id}`);
+    const data = await res.json();
+
+    return Object.assign(require(`../../cfg/guild.json`), data);
   }
 };
