fb0071112ff222fd91aa9757e05d336c9cb18ec4
Kyle Edwards <wingedasterisk@gmail.com>
2021-10-04 17:29:21 -0700

    more message handler stuff

--- vmodules/events/message.js
+++ vmodules/events/message.js
@@ -7,6 +7,15 @@ exports.run = async (message) => {
   const bot = memory.client;
   const member = message.member;
 
+  // not ready to work
   if (bot.booting) return;
+  // not a message we can/should process.
   if (message.author.bot || message.author.system || message.system || message.type !== `DEFAULT`) return;
+  // bot is in debug mode and the message author is not whitelisted
+  if (bot.debug && ![bot.cfg.dev.discord_user_id, ...bot.cfg.dev.user_whitelist].includes(message.author.id)) return;
+
+  const gcfg = await bot.managers.configs.get(message.guild);
+
+  log(`messij recieved`);
+  log(gcfg);
 };
