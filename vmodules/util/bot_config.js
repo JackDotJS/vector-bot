70dcb9902b95aa1c20d6cdeb0411f078cc159f8c
Kyle Edwards <wingedasterisk@gmail.com>
2021-10-18 15:21:04 -0700

    bot cfg util: use deep merge util

--- vmodules/util/bot_config.js
+++ vmodules/util/bot_config.js
@@ -1,12 +1,14 @@
 const cfg = require(`../../cfg/bot.json`);
 const dcfg = require(`../../cfg/bot_debug.json`);
+const merge = require(`../util/deep_merge.js`);
 
 module.exports = (debug) => {
   if (debug) {
     // if bot is running in debug mode, load debug config and overwrite main cfg properties.
     // this means the main config is used as a base, and the debug config only needs to specify
     // properties that need to be changed.
-    Object.assign(cfg, dcfg);
+
+    return merge(cfg, dcfg);
   }
 
   return cfg;
