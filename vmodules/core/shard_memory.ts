e210c0182d501189832b87b8fe117d5c841be0db
akii0008 <samir.js.zip@gmail.com>
2021-10-19 00:03:18 -0400

    shard_memory: update interface

--- vmodules/core/shard_memory.ts
+++ vmodules/core/shard_memory.ts
@@ -9,7 +9,10 @@ export interface ShardMemory {
   client: Vector | null,
   commands: Command[] | null,
   lang: {
-    default: string | null,
+    default: {
+      name: string,
+      phrases: object // lang file that is too large to possibly fit into an interface. just pray.
+    },
     index: {
       name: string,
       phrases: object // lang file that is too large to possibly fit into an interface. just pray.
