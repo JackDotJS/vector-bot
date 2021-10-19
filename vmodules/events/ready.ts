d2c7c8026989d97bb3cb5b4fba736c3188739ecc
akii0008 <samir.js.zip@gmail.com>
2021-10-19 18:04:48 -0400

    events/ready: ts-ified

--- vmodules/events/ready.ts
+++ vmodules/events/ready.ts
@@ -1,7 +1,8 @@
-const log = require(`../util/logger.js`).write;
+import { write as log } from '../util/logger';
+import Vector from '../classes/client';
 
-exports.name = `ready`;
-exports.run = (client) => {
+export const name =  `ready`;
+export const run = (client: Vector) => {
   log(`Successfully connected to Discord API.`, `info`);
   log(`This shard is handling ${client.guilds.cache.size} guild(s)!`, `info`);
 };
