2d186752b91754fd1dda93878e33563b8abad61c
akii0008 <samir.js.zip@gmail.com>
2021-10-19 18:04:42 -0400

    events/message: ts-ified

--- vmodules/events/message.ts
+++ vmodules/events/message.ts
@@ -1,13 +1,15 @@
-const util = require(`util`);
-const path = require(`path`);
-const djs = require(`discord.js`);
-const wink = require(`jaro-winkler`);
-const memory = require(`../core/shard_memory.js`);
+import util from 'util';
+import path from 'path';
+import djs, { Message } from 'discord.js';
+import wink from 'jaro-winkler'; // https://github.com/DefinitelyTyped/DefinitelyTyped/pull/56607
+import memory from '../core/shard_memory.js';
+import Vector from '../classes/client';
+import BotConfig from '../util/interfaces/BotConfig';
 const log = require(`../util/logger.js`).write;
 
 exports.name = `messageCreate`;
-exports.run = async (message) => {
-  const bot = memory.client;
+exports.run = async (message: Message) => {
+  const bot = memory.client!;
 
   // not ready to work
   if (bot.booting) return;
@@ -19,7 +21,7 @@ exports.run = async (message) => {
   if (message.channel.type === `DM`) {
     // can't use locale in DM since there's no access to guild configs
     const embed = new djs.MessageEmbed()
-      .setAuthor(`Vector`, await bot.managers.assets.getIcon(`info`, bot.cfg.colors.default))
+      .setAuthor(`Vector`, await bot.managers.assets.getIcon(`info`, (bot.cfg.colors.default as string)))
       .setTitle(`A Discord bot for advanced moderation & server management.`)
       .setDescription(`[placeholder url](https://discord.gg/s5nQBxFPp2)`)
       .setColor(bot.cfg.colors.default);
@@ -27,18 +29,21 @@ exports.run = async (message) => {
     return message.channel.send({ embeds: [embed] });
   }
 
+  if (message.guild === null || !message.guild.available)
+    return log('Tried to read message information from a message that was in an unavailable guild.', 'warn');
+    
   const gcfg = await bot.managers.configs.get(message.guild);
   const perms = null; // todo
 
   // checks if the input starts with the command prefix, immediately followed by valid characters.
   const valid_cmd = new RegExp(`^(\\${gcfg.commands.prefixes.join(`|\\`)})[a-zA-Z0-9]+`).test(message.content);
 
-  if (valid_cmd) return handle_command(bot, message, gcfg, perms);
+  if (valid_cmd) return handle_command(bot, message, gcfg, /* perms */);
 };
 
-async function handle_command(bot, message, gcfg, perms) {
+async function handle_command(bot: Vector, message: Message, gcfg: BotConfig, /* perms */): Promise<void> {
   const input_args = message.content.slice(1).trim().split(/ +/g);
-  const input_cmd = input_args.shift().toLowerCase();
+  const input_cmd = input_args.shift()!.toLowerCase();
 
   const cmd = await bot.managers.commands.get(input_cmd);
 
@@ -47,7 +52,7 @@ async function handle_command(bot, message, gcfg, perms) {
       .setAuthor(bot.managers.locale.text(
         `cmd.unknown.title`,
         gcfg.lang
-      ), await bot.managers.assets.getIcon(`info`, bot.cfg.colors.default))
+      ), await bot.managers.assets.getIcon(`info`, (bot.cfg.colors.default as string)))
       .setFooter(bot.managers.locale.text(
         `cmd.unknown.help`,
         gcfg.lang,
@@ -55,12 +60,13 @@ async function handle_command(bot, message, gcfg, perms) {
       ))
       .setColor(bot.cfg.colors.default);
 
-    if (memory.commands.length === 0) return message.reply({ embeds: [embed] });
+    if (memory.commands === null || memory.commands.length === 0)
+      return message.reply({ embeds: [embed] });
 
     const ratings = [];
 
     for (const searchcmd of memory.commands) {
-      if (searchcmd.guilds != null && searchcmd.guilds.includes(message.guild.id)) continue;
+      if (searchcmd.guilds != null && searchcmd.guilds.includes(message.guild!.id)) continue;
       if (gcfg.commands.hidden.includes(searchcmd.name)) continue;
       if (gcfg.commands.disabled.includes(searchcmd.name)) continue;
 
@@ -87,40 +93,61 @@ async function handle_command(bot, message, gcfg, perms) {
   };
 
   // command doesnt exist
-  if (cmd == null) return unknown();
+  if (cmd == null) {
+    unknown();
+    return;
+  }
 
   // command is disabled by server manager
-  if (gcfg.commands.disabled.includes(cmd.name)) return unknown();
+  if (gcfg.commands.disabled.includes(cmd.name)) {
+    unknown();
+    return;
+  }
 
   // command cannot be used in this server
-  if (cmd.guilds != null && !cmd.guilds.includes(message.guild.id)) return unknown();
+  if (cmd.guilds != null && !cmd.guilds.includes(message.guild!.id)) {
+    unknown();
+    return;
+  }
 
   try {
-    await cmd.run(message, input_args, gcfg);
+    await cmd.run!(message, input_args, gcfg);
   }
   catch (err) {
-    log(err.stack, `error`);
+
+    const error = err instanceof Error ? err : (err as string);
+
+    log(error, `error`);
 
     // the following is a bunch of code to print out a clean and simplified version of the error stack
     // of course the full, unedited stack will still appear in the bot's console
     // this code just makes stack traces easier to read without flooding text channels
-    let formatted = [err.toString()];
+    let formatted = [error.toString()];
 
     const file_rgx = new RegExp(`${path.parse(process.cwd()).root}\\[^,]+\\d+:\\d+`);
     const trace_rgx = /(?<=at\s)[^\r\n\t\f\v(< ]+/;
     const evaltrace_rgx = /(?<=<anonymous>):\d+:\d+/;
 
-    for (const line of err.stack.split(`\n`)) {
+    for (const line of err instanceof Error
+      ? err.stack!.split(`\n`)
+      : (err as string).split('\n')
+    ) {
       if (line.includes(`node_modules`)) break;
 
       let file = line.match(file_rgx);
       let trace = line.match(trace_rgx);
       let evalTrace = line.match(evaltrace_rgx);
 
+      let traceString: string | undefined = undefined;
+      let evalTraceString: string | undefined = undefined;
+
       if (file == null) continue;
-      file = file[0];
-      if (trace) trace = trace[0];
-      if (evalTrace) evalTrace = evalTrace[0];
+
+      const fileString = file[0];
+      if (trace)
+        traceString = trace[0];
+      if (evalTrace)
+        evalTraceString = evalTrace[0];
 
       // just to be fucking sure
       file_rgx.lastIndex = 0;
@@ -129,11 +156,11 @@ async function handle_command(bot, message, gcfg, perms) {
 
       let str = ``;
 
-      const fileshort = file.replace(process.cwd(), `~`);
+      const fileshort = fileString.replace(process.cwd(), `~`);
 
-      if (trace && trace !== file) {
-        if (trace === `eval`) {
-          str += trace + evalTrace;
+      if (traceString && traceString !== fileString) {
+        if (traceString === `eval`) {
+          str += traceString + evalTraceString;
           formatted.push(str);
           formatted.push(fileshort);
         } else if (evalTrace) {
@@ -150,14 +177,14 @@ async function handle_command(bot, message, gcfg, perms) {
     }
 
     // remove duplicate lines and combine into final string
-    formatted = [...new Set(formatted)].join(`\n    at `);
+    const formattedDeDuped = [...new Set(formatted)].join(`\n    at `);
 
     const embed = new djs.MessageEmbed()
-      .setAuthor(`Something went wrong.`, await bot.managers.assets.getIcon(`error`, bot.cfg.colors.error))
+      .setAuthor(`Something went wrong.`, await bot.managers.assets.getIcon(`error`, (bot.cfg.colors.error as string)))
       .setColor(bot.cfg.colors.error)
       .setDescription([
         `\`\`\`diff`,
-        `- ${formatted}`,
+        `- ${formattedDeDuped}`,
         `\`\`\``
       ].join(`\n`))
       .addField(`Keep getting errors?`, `Please be sure to report them at https://github.com/JackDotJS/vector-bot`);
