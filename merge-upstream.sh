c3d52465ee3c442ca3988206d67a50e25dbb9465
akii0008 <samir.js.zip@gmail.com>
2022-06-03 22:09:27 -0400

    edited merge upstream script a little

--- merge-upstream.sh
+++ merge-upstream.sh
@@ -3,7 +3,7 @@
 # Stop at command failures. Why this isn't default, I will never know.
 set -e
 
-git checkout dev
-git pull upstream dev
-git commit -m "Merge upstream changes"
-git checkout -
+# https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/working-with-forks/syncing-a-fork#syncing-a-fork-from-the-command-line
+git fetch upstream;
+git checkout ts-dev;
+git merge upstream/dev;
