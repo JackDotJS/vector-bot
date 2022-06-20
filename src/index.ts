209cad994bd490ca09589aa64ed25974ac79ce95
Kyle Edwards <wingedasterisk@gmail.com>
2022-06-20 10:05:14 -0700

    track app resets

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import * as util from 'util';
 import * as pkg from '../package.json';
 
 
@@ -19,3 +20,50 @@ for (const item of mkdirs) {
     fs.mkdirSync(item, { recursive: true });
   }
 }
+
+try {
+  // try making file if it does not exist
+  const data = {
+    logins: 1,
+    time: new Date().getTime()
+  };
+
+  fs.writeFileSync(`./data/resets`, JSON.stringify(data), { encoding: `utf8`, flag: `ax` });
+}
+catch (e: any) {
+  if (e.code !== `EEXIST`) {
+    throw e;
+  }
+
+  const oldData = fs.readFileSync(`./data/resets`, { encoding: `utf8` });
+
+  let json = {
+    logins: 0,
+    time: 0
+  };
+
+  try {
+    // attempt to parse as JSON.
+    // if the data is corrupt or otherwise invalid, we'll simply overwrite it with some default values.
+    json = JSON.parse(oldData);
+
+    console.log(`filedata successful read`);
+  }
+  catch (je) {}
+
+  // if it's been more than an hour, reset the login count
+  const now = new Date().getTime();
+  if (now - json.time > (1000 * 60 * 60)) {
+    json.logins = 0;
+    json.time = now;
+  }
+
+  json.logins++;
+
+  console.log(`login count: ${json.logins}`);
+  
+  fs.writeFileSync(`./data/resets`, JSON.stringify(json), { encoding: `utf8` });
+}
+
+// we did it reddit
+console.log(`:)`);
