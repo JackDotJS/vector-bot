591785659fd9026e3dd0bad7bcf4c8bf9bfbee98
Kyle Edwards <wingedasterisk@gmail.com>
2022-06-20 09:01:00 -0700

    make some folders on app start

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import * as fs from 'fs';
+import * as pkg from '../package.json';
+
+
+process.title = `Vector Bot ${pkg.version}`;
+
+// create directories that may or may not exist because Git(TM)
+const mkdirs = [
+  `../.local`,
+  `../data`,
+  `../data/archive`,
+  `../logs/crash`,
+  `../logs/archive`,
+  `../logs/all`
+];
+
+for (const item of mkdirs) {
+  if (!fs.existsSync(item)) {
+    fs.mkdirSync(item, { recursive: true });
+  }
+}
