00684f0cd310c01713e0549ead93f7f1a940f790
akii0008 <samir.js.zip@gmail.com>
2022-06-21 17:31:28 -0400

    bot: login to discord

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,6 @@
 import { Client, Intents } from 'discord.js';
 import Logger from './util/logger';
+import keys from '../cfg/keys.json';
 
 export const logger = new Logger();
 
@@ -14,4 +15,9 @@ const client = new Client({
   ]
 });
 
-
+client.login(keys.discord)
+  .then(() => logger.log(`Successfully logged in as ${client.user?.tag}!`))
+  .catch(error => {
+    logger.fatal(`Unexpected error when logging into Discord: ${error}`);
+    process.exit(1);
+  });
